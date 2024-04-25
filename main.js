5d376db358999467a9ad455fdb9b34d007de6b54
bilalbuttnazbutt <164246858+bilalbuttnazbutt@users.noreply.github.com>
2024-04-25 19:04:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//storing the famous quote and its author
+var quote = "Nothinng is impossible because impossible itself I M POSSIBLE";
+var author = "naz butt";
+//printing the message
+console.log("\"".concat(quote, "\" - ").concat(author));
---
+++ main.ts
@@ -0,0 +1,9 @@
+//storing the famous quote and its author
+
+let quote:string = "Nothinng is impossible because impossible itself I M POSSIBLE"
+
+let author:string = "naz butt";
+
+//printing the message
+
+console.log(`"${quote}" - ${author}`);
